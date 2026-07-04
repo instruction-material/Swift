f81daee8cd46eaf666115e1556ecf96b05c89120
Jacob Anderson <jacobdanderson@gmail.com>
2026-07-04 15:16:20 -0400

    Apply coding standard to Swift app solutions

--- SAD-26-sad14-media-maps-and-device-features-supplemental-2/solution/App.swift
+++ SAD-26-sad14-media-maps-and-device-features-supplemental-2/solution/App.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
+/****************
+*   APP ENTRY   *
+****************/
+
+// Provide the SwiftUI application entry point
 @main
 struct SolutionApp: App {
+	// Build the root application scene
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
