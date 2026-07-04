dbd1b81544d5d668565c5249e3ec7756132deead
Jacob Anderson <jacobdanderson@gmail.com>
2026-07-04 18:29:11 -0400

    Apply coding standard markers to solutions

--- SAD4-Map-Places-App/solution/ContentView.swift
+++ SAD4-Map-Places-App/solution/ContentView.swift
@@ -1,3 +1,9 @@
+/**************************
+*   CODING STANDARD   *
+**************************/
+
+// Use named constants, descriptive names, and purpose comments before nontrivial scopes
+
 import MapKit
 import SwiftUI
 
--- SAD5-Simple-Tracker-App/solution/ContentView.swift
+++ SAD5-Simple-Tracker-App/solution/ContentView.swift
@@ -1,3 +1,9 @@
+/**************************
+*   CODING STANDARD   *
+**************************/
+
+// Use named constants, descriptive names, and purpose comments before nontrivial scopes
+
 import SwiftUI
 
 struct TrackerItem: Identifiable, Codable {
